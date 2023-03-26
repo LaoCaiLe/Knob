3324420b3352940e3e94298fa519a7fa267a4e4a
Laocaile <1754318216@qq.com>
2023-03-26 14:36:32 +0800

    Revert "fix conflicts"
    This reverts commit dc3dffb1eaef9dce90bb93d08bde1f8e66af02f7, reversing
    changes made to eced5f5254597a52fbe092b94f33eb0d4bc291bb.

--- src/motor.cpp
+++ src/motor.cpp
@@ -3,8 +3,8 @@
 #include "motor.h"
 #include "oled.h"
 
-BLDCMotor motor = BLDCMotor(7,11);
-// BLDCMotor motor = BLDCMotor(11,12.5);
+// BLDCMotor motor = BLDCMotor(7,11);
+BLDCMotor motor = BLDCMotor(11,12.5);
 BLDCDriver3PWM driver = BLDCDriver3PWM(MOTOR_PWM1_PIN, MOTOR_PWM2_PIN, MOTOR_PWM3_PIN, MOTOR_EN1_PIN, MOTOR_EN2_PIN, MOTOR_EN3_PIN);
 MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
 TwoWire I2Cone = TwoWire(1);
@@ -17,47 +17,38 @@ void Motion::init()
 	motor.linkSensor(&sensor);	
 
 
-	driver.voltage_power_supply = 12;
+	driver.voltage_power_supply = 8;
 	driver.init();
 	motor.linkDriver(&driver);
 
 
 	motor.current_limit = 0.3;
-    // motor.voltage_limit = 12;
-    motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
+    // motor.voltage_limit = 3;
+	motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
     motor.torque_controller = TorqueControlType::voltage;
-    motor.controller = MotionControlType::angle;
+    motor.controller = MotionControlType::torque;
   	motor.sensor_direction = Direction::CCW;
-	// motor.zero_electric_angle  = 5.3490;
-	motor.zero_electric_angle  = 2.5173;
+	motor.zero_electric_angle  = 5.3152;
 
-	// motor.PID_velocity.P = 0.1;
-	// motor.PID_velocity.I = 4;
-    motor.PID_velocity.P = 0.008;
-    motor.PID_velocity.I = 0.2;
+	motor.PID_velocity.P = 0.1;
+	motor.PID_velocity.I = 4;
 	motor.PID_velocity.D = 0.000;
 	motor.PID_velocity.output_ramp = 1000;
+	motor.LPF_velocity.Tf = 0.01;
+	motor.PID_velocity.limit = 0.2;
 
-	motor.LPF_velocity.Tf = 0.1;
-	// motor.PID_velocity.limit = 0.2;
-
-
-	// motor.P_angle.P = 20;
-	motor.P_angle.P = 10;
+	motor.P_angle.P = 20;
 	motor.P_angle.I = 0;
   	motor.P_angle.D = 0.0;
 	motor.P_angle.output_ramp = 1000.0;
-	motor.P_angle.limit = 60;
+	motor.P_angle.limit = 10;
 	motor.LPF_angle.Tf = 0.01;
-
-  
 
 	// 初始化电机
 	motor.init();
 	motor.initFOC();
-    Serial.begin(115200);
 
-    motor.PID_velocity.limit = 0.1;
+    motor.PID_velocity.limit = 0.08;
 
 	zero_angle = sensor.getAngle()/PI*180.0f;
     target_angle = zero_angle;
@@ -66,30 +57,27 @@ void Motion::init()
 
 void Motion::task_motor(void)
 {
-    motor.controller = MotionControlType::angle;    
 	
 	while(1)
 	{
-		// sensor.update();        
+		sensor.update();        
         now_angle = sensor.getAngle()/PI*180.0f;
         real_angle  = now_angle - zero_angle;
-
-		// vTaskDelay(1);
-        // position_check(-120,120,10);
-        // shake_mode(-145, 145, MOTOR_SHAKE_LEVEL_LOW);
-
+		vTaskDelay(1);
+        // position_check(-45,45,10);
+        shake_mode(-45, 45, MOTOR_SHAKE_LEVEL_LOW);
         motor.loopFOC();
-        // if( motor.controller == MotionControlType::angle)
-        // {
-        //     // if(target_angle>0 && err_angle<0)
-        //     //     err_angle = -err_angle;
-        //     // if(target_angle<0 && err_angle>0)
-        //     //     err_angle = -err_angle;
-        //     motor.move(-(target_angle / 180.0f * PI+err_angle ));
-        // }
-        // else
+        if( motor.controller == MotionControlType::angle)
+        {
+            if(target_angle>0 && err_angle<0)
+                err_angle = -err_angle;
+            if(target_angle<0 && err_angle>0)
+                err_angle = -err_angle;
+            motor.move(-(target_angle / 180.0f * PI+err_angle ));
+        }
+        else
             motor.move(target_angle);
-    }
+        }
   
 }
 
@@ -121,6 +109,7 @@ void Motion::position_check(int min_angle, int max_angle, int count)
     else
 	    digitalWrite(2, LOW);
 
+    motor.controller = MotionControlType::angle;    
 }
 
 
@@ -205,9 +194,7 @@ int Motion::shake_mode(int min_angle, int max_angle, motor_shake_level shake_lv)
         motor.controller = MotionControlType::torque;
         last_zhendong = false;
     }
-
-    // motor.loopFOC();
-
+    motor.loopFOC();
     // motor.move(target_angle);
     return 0 ;
 }
