be95c8eea79a51a64f1a8efa7563a67deced4c2e
Laocaile <1754318216@qq.com>
2023-07-07 18:01:42 +0800

    Change the indentation of tabs to Spaces

--- Software/include/led.h
+++ Software/include/led.h
@@ -8,16 +8,16 @@
 class ColorLed: public WS2812FX
 {
 public: 
-	ColorLed() = default;
-	ColorLed(uint8_t _pin, uint8_t _count) : WS2812FX(_pin, _count, NEO_GBR + NEO_KHZ800), pin(_pin), count(_count){}
-	~ColorLed() = default;
-	void init();
-	void task();
-	void set_color(uint8_t Brightness, uint32_t colors[]);
+    ColorLed() = default;
+    ColorLed(uint8_t _pin, uint8_t _count) : WS2812FX(_pin, _count, NEO_GBR + NEO_KHZ800), pin(_pin), count(_count){}
+    ~ColorLed() = default;
+    void init();
+    void task();
+    void set_color(uint8_t Brightness, uint32_t colors[]);
 
 private:
-	uint8_t pin = -1;
-	uint8_t count = -1;
+    uint8_t pin = -1;
+    uint8_t count = -1;
 
 };
 
