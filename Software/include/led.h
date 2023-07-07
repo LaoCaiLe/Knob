125e47f335d93ff8470d1df107302aeaa15d872d
Laocaile <1754318216@qq.com>
2023-07-07 17:49:54 +0800

    add ws2812b led driver code

---
+++ Software/include/led.h
@@ -0,0 +1,24 @@
+#ifndef LED_H
+#define LED_H
+#include <WS2812FX.h>
+
+#define LED_COUNT 8
+#define LED_PIN 2
+
+class ColorLed: public WS2812FX
+{
+public: 
+	ColorLed() = default;
+	ColorLed(uint8_t _pin, uint8_t _count) : WS2812FX(_pin, _count, NEO_GBR + NEO_KHZ800), pin(_pin), count(_count){}
+	~ColorLed() = default;
+	void init();
+	void task();
+	void set_color(uint8_t Brightness, uint32_t colors[]);
+
+private:
+	uint8_t pin = -1;
+	uint8_t count = -1;
+
+};
+
+#endif // !LED_H
---
+++ Software/src/led.cpp
@@ -0,0 +1,22 @@
+#include "led.h"
+
+void ColorLed::init()
+{
+	WS2812FX::init();
+}
+void ColorLed::task()
+{
+	if(pin == -1 || count == -1)
+		return;
+	this->service();
+}
+
+void ColorLed::set_color(uint8_t Brightness, uint32_t colors[])
+{
+	if(pin == -1 || count == -1)
+		return;
+	this->setBrightness(Brightness);
+	this->setSegment(0, 0, count - 1, FX_MODE_STATIC, colors, 100, false);
+	this->start();
+}
+
