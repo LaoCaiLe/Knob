cfc78e3ef2945ee0f8bad1c46fbaef9543214bf0
Laocaile <1754318216@qq.com>
2023-07-12 21:36:58 +0800

    fixing ws2812b led error

--- Software/include/led.h
+++ Software/include/led.h
@@ -7,11 +7,10 @@
 
 class ColorLed: public WS2812FX
 {
-public: 
+public:
     ColorLed() = default;
-    ColorLed(uint8_t _pin, uint8_t _count) : WS2812FX(_pin, _count, NEO_GBR + NEO_KHZ800), pin(_pin), count(_count){}
+    ColorLed(uint8_t _pin, uint8_t _count) : WS2812FX(_count, _pin, NEO_GBR + NEO_KHZ800), pin(_pin), count(_count){}
     ~ColorLed() = default;
-    void init();
     void task();
     void set_color(uint8_t Brightness, uint32_t colors[]);
 
