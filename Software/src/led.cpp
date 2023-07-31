14441d9a080e27d5fd82e8f58f0a8b5835d81e76
Laocaile <1754318216@qq.com>
2023-07-31 23:14:16 +0800

    move lvgl lib to .pio

--- Software/src/led.cpp
+++ Software/src/led.cpp
@@ -3,42 +3,42 @@
 
 extern Scale scale;
 extern int option_index;
-
+extern int max_option_index;
 void ColorLed::task()
 {
     static bool button = true;
     while(1)
     {
-        if(!scale.is_Init_Thresh)
-        {
-            static long all = 0;
-            static char i = 0;
-            all += scale.read();
-            i++;
-            if(i==20)
-            {
-                all = 0;
-            }
-            if(i==70)
-            {
-                scale.threshold = all / 50 -100;
-                // Serial.printf("threshold = %d\n", scale.threshold);
-                scale.is_Init_Thresh = true;
-            }
-        }
-        else
+        // if(!scale.is_Init_Thresh)
+        // {
+        //     static long all = 0;
+        //     static char i = 0;
+        //     all += scale.read();
+        //     i++;
+        //     if(i==20)
+        //     {
+        //         all = 0;
+        //     }
+        //     if(i==70)
+        //     {
+        //         scale.threshold = all / 50 -100;
+        //         // Serial.printf("threshold = %d\n", scale.threshold);
+        //         scale.is_Init_Thresh = true;
+        //     }
+        // }
+        // else
         {
 
             if(pin == -1 || count == -1)
                 return;
             // Serial.printf("value:%d\n", scale.press_value);
-
+            
             if(button&&scale.is_press())
             {
                 set_color(100, BLUE, FX_MODE_STATIC);
                 button = false;
                 option_index++;
-                option_index %= 4;
+                option_index %= max_option_index;
             }
             else if(!scale.is_press())
             {
@@ -46,7 +46,7 @@ void ColorLed::task()
                 button = true;
             }
             this->service();
-            // vTaskDelay(5);
+            vTaskDelay(200);
         }
 
     }
--- Software/src/scale.cpp
+++ Software/src/scale.cpp
@@ -7,9 +7,17 @@ void Scale::init(byte pin_dout, byte pin_sck)
 
 bool Scale::is_press()
 {
-    press_value = this->read();
-    if (press_value < threshold)
-        return true;
+    // press_value = this->read();
+    // if (press_value < threshold)
+    //     return true;
+    // else
+    //     return false;
+    if(touchRead(T8) < 10)
+    {
+        vTaskDelay(10);
+        if(touchRead(T8) < 10)
+            return true;
+    }
     else
         return false;
 }
